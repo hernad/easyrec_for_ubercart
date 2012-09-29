585545b4bfd4a64bc59042621968ac69081bab26
Ernad Husremovic <hernad@bring.out.ba>
2012-09-29 12:11:48 +0200

    prvo ucitati .js pa onda u html inline staviti javascript code

--- easyrec/easyrec.js
+++ easyrec/easyrec.js
@@ -18,6 +18,8 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  */
 
+// debugger;
+
 var defaults = {
         drawingCallback: "drawRecommendationList",
         actionCallback: "void",
